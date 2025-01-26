6ce951ee53f5edf092cc5ccbe34bb2618265c8b1
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-01-27 01:52:53 +0300

    rest

--- src/main/java/ru/gocinema/server/repositories/HallPlaceRepository.java
+++ src/main/java/ru/gocinema/server/repositories/HallPlaceRepository.java
@@ -1,5 +1,6 @@
 package ru.gocinema.server.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +8,7 @@ import ru.gocinema.server.model.HallPlace;
 
 @Repository
 public interface HallPlaceRepository extends CrudRepository<HallPlace, Integer> {
+    @Modifying
     @Query("delete from HallPlace p where p.hall.id = :hallId")
     void deleteAllByHallId(int hallId);
 }
