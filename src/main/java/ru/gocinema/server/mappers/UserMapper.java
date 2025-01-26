f026d6884982be05ae6cc8647705f70be21b9c2b
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-01-26 21:47:20 +0300

    data

--- src/main/java/ru/gocinema/server/mappers/UserMapper.java
+++ src/main/java/ru/gocinema/server/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package ru.gocinema.server.mappers;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.gocinema.restapi.model.User;
 import ru.gocinema.restapi.model.UserParameters;
 
@@ -10,5 +11,6 @@ public interface UserMapper {
     User map(ru.gocinema.server.model.User source);
     List<User> map(Iterable<ru.gocinema.server.model.User> source);
 
+    @Mapping(target = "password", ignore = true)
     ru.gocinema.server.model.User map(UserParameters parameters);
 }
