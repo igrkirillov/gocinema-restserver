675de70beccb11cd860b5c3250fe42deb4b96a49
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-01-26 21:54:47 +0300

    data

--- src/main/java/ru/gocinema/server/services/PasswordServiceImpl.java
+++ src/main/java/ru/gocinema/server/services/PasswordServiceImpl.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PasswordServiceImpl implements PasswordService {
 
-    private static final String SALT = "gocimena";
+    private static final String MD5_SALT = "$1$gocim";
 
     @Override
     public String sign(String password) {
-        return Md5Crypt.md5Crypt(password.getBytes(StandardCharsets.UTF_8), SALT);
+        return Md5Crypt.md5Crypt(password.getBytes(StandardCharsets.UTF_8), MD5_SALT);
     }
 }
