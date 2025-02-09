37a2898b920511915aa8de8e43ad537031a3fb02
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-02-09 19:00:37 +0300

    hall places into hall

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,8 +10,24 @@ values ('customer', 'customer', 'CUSTOMER');
 
 merge into hall(name, cols, rows)
 key(name)
-values ('Зал 1', 0, 0);
+values ('Зал 1', 2, 2);
 
 merge into hall(name, cols, rows)
 key(name)
 values ('Зал 2', 0, 0);
+
+merge into hall_place(hall_id, _row, _col, is_vip, is_blocked)
+key(hall_id, _row, _col)
+values ((select id from hall where name = 'Зал 1'), 0, 0, true, false);
+
+merge into hall_place(hall_id, _row, _col, is_vip, is_blocked)
+key(hall_id, _row, _col)
+values ((select id from hall where name = 'Зал 1'), 0, 1, false, true);
+
+merge into hall_place(hall_id, _row, _col, is_vip, is_blocked)
+key(hall_id, _row, _col)
+values ((select id from hall where name = 'Зал 1'), 1, 0, false, false);
+
+merge into hall_place(hall_id, _row, _col, is_vip, is_blocked)
+key(hall_id, _row, _col)
+values ((select id from hall where name = 'Зал 1'), 1, 1, false, false);
